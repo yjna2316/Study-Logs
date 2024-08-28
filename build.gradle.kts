c22d8d7b610b222900ed01d7acdb38cfbe4fb71c
Yoonji Na <yjna2316@naver.com>
2024-08-29 06:56:20 +0900

    feat: upgrade kotlin, gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
-	kotlin("jvm") version "1.9.24"
-	kotlin("plugin.spring") version "1.9.24"
+	kotlin("jvm") version "2.0.0"
+	kotlin("plugin.spring") version "2.0.0"
 }
 
 group = "com.kopring"
 version = "0.0.1-SNAPSHOT"
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+kotlin {
+	jvmToolchain(21)
 }
 
 repositories {
@@ -32,6 +32,34 @@ kotlin {
 	}
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+// https://github.com/kotest/kotest/releases
+val kotestVersion = "5.9.1"
+
+// Apply the plugins to all submodules
+subprojects {
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+
+	dependencies {
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+		testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+		testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+		testImplementation ("io.kotest:kotest-property:$kotestVersion")
+		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	}
+
+	tasks.withType<Test>().configureEach {
+		useJUnitPlatform()
+	}
+}
+
+tasks.named<BootJar>("bootJar") {
+	enabled = false
+}
+
+tasks.named<Jar>("jar") {
+	enabled = true
 }
