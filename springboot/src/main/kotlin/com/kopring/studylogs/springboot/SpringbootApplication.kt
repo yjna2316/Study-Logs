0030eb6c6c41e930e0c32897989f3817aa527c33
Yoonji Na <yjna2316@naver.com>
2024-09-22 23:16:23 +0900

    feat: @EnableSchedular 어노테이션 추가

--- springboot/src/main/kotlin/com/kopring/studylogs/springboot/SpringbootApplication.kt
+++ springboot/src/main/kotlin/com/kopring/studylogs/springboot/SpringbootApplication.kt
@@ -2,7 +2,9 @@ package com.kopring.studylogs.springboot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class SpringbootApplication
 
